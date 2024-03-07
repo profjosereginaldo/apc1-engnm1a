595aa0f6afc9da61dca5658d4285478353a76222
Prof. José Reginaldo <86785375+profjosereginaldo@users.noreply.github.com>
2024-03-07 00:47:36 +0000

    codigos da aula2 e pratica1

---
+++ aulas/aula02/constantes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define SEXO_FEMININO 2
+
+int main() {
+  const float PI = 3.14; 
+
+  // PI = 3.1415; Num pode!!!!
+  const int SEXO_MASCULINO = 1;
+  int sexo = SEXO_MASCULINO;
+  
+  return 0;
+}
